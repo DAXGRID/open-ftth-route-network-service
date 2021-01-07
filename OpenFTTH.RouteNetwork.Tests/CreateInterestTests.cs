35ce2f9f77f42ae243eb2d4c80a4db953ae331ca
Jesper Ladegaard <jesper@dax.dk>
2021-01-07 15:16:09 +0100

    tests added

--- OpenFTTH.RouteNetwork.Tests/CreateInterestTests.cs
+++ OpenFTTH.RouteNetwork.Tests/CreateInterestTests.cs
@@ -43,7 +43,7 @@ namespace OpenFTTH.RouteNetwork.Tests
         public async void CreateValidWalkOfInterestUsingThreeSegments_ShouldReturnSuccess()
         {
             // Route network subset used in this test:
-            // (CO_1) <- (S1) -> (HH_1) -> (S2) -> (HH_2) -> (S4) -> (CC_1)
+            // (CO_1) <- (S1) -> (HH_1) <- (S2) -> (HH_2) <- (S4) -> (CC_1)
             var interestId = Guid.NewGuid();
 
             var walk = new RouteNetworkElementIdList() { testNetwork.S1, testNetwork.S2, testNetwork.S4 };
@@ -63,6 +63,68 @@ namespace OpenFTTH.RouteNetwork.Tests
             Assert.Equal(testNetwork.HH_2, routeNodeQueryResult.Value.Walk[4]);
             Assert.Equal(testNetwork.S4, routeNodeQueryResult.Value.Walk[5]);
             Assert.Equal(testNetwork.CC_1, routeNodeQueryResult.Value.Walk[6]);
+        }
+
+        [Fact]
+        public async void CreateValidWalkOfInterestOverlappingSegments_ShouldReturnSuccess()
+        {
+            // Route network subset used in this test:
+            // (CO_1) <- (S1) -> (HH_1) <- (S2) -> (HH_2) <- (S4) -> (CC_1)
+            var interestId = Guid.NewGuid();
+
+            var walk = new RouteNetworkElementIdList() { testNetwork.S1, testNetwork.S2, testNetwork.S1 };
+
+            var createInterestCommand = new RegisterWalkOfInterestCommand(interestId, walk);
+
+            // Act
+            Result<RegisterWalkOfInterestCommandResult> routeNodeQueryResult = await testNetwork.CommandApi.HandleAsync(createInterestCommand);
+
+            // Assert command success and that the result include all three route network element ids
+            Assert.True(routeNodeQueryResult.IsSuccess);
+            Assert.Equal(7, routeNodeQueryResult.Value.Walk.Count);
+            Assert.Equal(testNetwork.CO_1, routeNodeQueryResult.Value.Walk[0]);
+            Assert.Equal(testNetwork.S1, routeNodeQueryResult.Value.Walk[1]);
+            Assert.Equal(testNetwork.HH_1, routeNodeQueryResult.Value.Walk[2]);
+            Assert.Equal(testNetwork.S2, routeNodeQueryResult.Value.Walk[3]);
+            Assert.Equal(testNetwork.HH_2, routeNodeQueryResult.Value.Walk[4]);
+            Assert.Equal(testNetwork.S1, routeNodeQueryResult.Value.Walk[5]);
+            Assert.Equal(testNetwork.CO_1, routeNodeQueryResult.Value.Walk[6]);
+        }
+
+        [Fact]
+        public async void CreateInvalidWalkOfInterestUsingOneNodeAndOneSegments_ShouldReturnFaliour()
+        {
+            // Route network subset used in this test:
+            // (CO_1) <- (S1)
+            var interestId = Guid.NewGuid();
+
+            var walk = new RouteNetworkElementIdList() { testNetwork.CO_1, testNetwork.S1 };
+
+            var createInterestCommand = new RegisterWalkOfInterestCommand(interestId, walk);
+
+            // Act
+            Result<RegisterWalkOfInterestCommandResult> routeNodeQueryResult = await testNetwork.CommandApi.HandleAsync(createInterestCommand);
+
+            // Assert
+            Assert.True(routeNodeQueryResult.IsFailure);
+        }
+
+        [Fact]
+        public async void CreateInvalidWalkOfInterestUsingTwoSeparatedSegments_ShouldReturnFaliour()
+        {
+            // Route network subset used in this test:
+            // (CO_1) <- (S1) -> (HH_1) hole in the walk here (HH_2) -> (S4) -> (CC_1)
+            var interestId = Guid.NewGuid();
+
+            var walk = new RouteNetworkElementIdList() { testNetwork.S1, testNetwork.S4 };
+
+            var createInterestCommand = new RegisterWalkOfInterestCommand(interestId, walk);
+
+            // Act
+            Result<RegisterWalkOfInterestCommandResult> routeNodeQueryResult = await testNetwork.CommandApi.HandleAsync(createInterestCommand);
+
+            // Assert
+            Assert.True(routeNodeQueryResult.IsFailure);
         }
 
 
