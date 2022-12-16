2b8dda3dbde3a8a6952ce9b1459360395e467597
Jesper Ladegaard <jesper@dax.dk>
2022-12-16 16:41:43 +0100

    store coordinates in a double array instead of geojson string; envelope functionality added

---
+++ OpenFTTH.RouteNetwork.Business/RouteElements/Model/Envelope.cs
@@ -0,0 +1,69 @@
+﻿namespace OpenFTTH.RouteNetwork.Business.RouteElements.Model
+{
+    public record class Envelope
+    {
+        public double MinX { get; set; }
+        public double MinY { get; set; }
+        public double MaxX { get; set; }
+        public double MaxY { get; set; }
+
+
+        public Envelope()
+        {
+            MinX = double.MaxValue;
+            MinY = double.MaxValue;
+            MaxX = double.MinValue;
+            MaxY = double.MinValue;
+        }
+
+        public Envelope(double minX, double minY, double maxX, double maxY)
+        {
+            MinX = minX;
+            MinY = minY;
+            MaxX = maxX;
+            MaxY = maxY;
+        }
+
+        public void ExpandToInclude(double x, double y)
+        {
+            if (MinX > x)
+            { 
+                MinX = x; 
+            }
+
+            if (MinY > y)
+            {
+                MinY = y;
+            }
+
+            if (MaxX < x)
+            {
+                MaxX = x;
+            }
+
+            if (MaxY < y)
+            {
+                MaxY = y;
+            }
+        }
+
+        public bool IsWithin(double[] coordArray)
+        {
+            for (int i = 0; i < coordArray.Length; i+=2)
+            {
+                if (!IsWithin(coordArray[i], coordArray[i + 1]))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public bool IsWithin(double x, double y) 
+        {
+            if (x >= MinX && x <= MaxX && y >= MinY && y <= MaxY)
+                return true;
+            else
+                return false;
+        }
+    }
+}
--- OpenFTTH.RouteNetwork.Business/RouteElements/Model/RouteSegment.cs
+++ OpenFTTH.RouteNetwork.Business/RouteElements/Model/RouteSegment.cs
@@ -2,21 +2,89 @@
 using OpenFTTH.Events.Core.Infos;
 using OpenFTTH.Events.RouteNetwork.Infos;
 using System;
+using System.Globalization;
+using System.Text;
 
 namespace OpenFTTH.RouteNetwork.Business.RouteElements.Model
 {
     public class RouteSegment : GraphEdge, IRouteSegment
     {
-        public string Coordinates { get; set; }
         public RouteSegmentInfo? RouteSegmentInfo { get; set; }
         public NamingInfo? NamingInfo { get; set; }
         public LifecycleInfo? LifecycleInfo { get; set; }
         public SafetyInfo? SafetyInfo { get; set; }
         public MappingInfo? MappingInfo { get; set; }
 
+        public double[] CoordArray;
+
         public RouteSegment(Guid id, string coordinates, RouteNode fromNode, RouteNode toNode) : base(id, fromNode, toNode)
         {
-            this.Coordinates = coordinates;
+            CoordArray = DoubleArrayFromCoordinateString(coordinates);
+
+            if (CoordArray.Length < 4)
+            {
+                throw new ApplicationException($"Invalid linestring: '${coordinates}' Expected at least two coordinates.");
+            }
+        }
+
+        public string Coordinates { 
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+
+                sb.Append('[');
+
+                for (int i = 0; i < CoordArray.Length; i+=2) 
+                {
+                    if (sb.Length > 1)
+                        sb.Append(',');
+
+                    sb.Append('[');
+                    sb.Append(CoordArray[i].ToString(CultureInfo.InvariantCulture));
+                    sb.Append(',');
+                    sb.Append(CoordArray[i+1].ToString(CultureInfo.InvariantCulture));
+                    sb.Append(']');
+                }
+
+                sb.Append(']');
+
+                return sb.ToString();
+            }
+
+            set
+            {
+                CoordArray = DoubleArrayFromCoordinateString(value);
+            }
+        }
+
+        public Envelope Extent
+
+        {
+            get
+            {
+                var extend = new Envelope();
+
+                for (int i = 0; i < CoordArray.Length; i+=2)
+                {
+                    Extent.ExpandToInclude(CoordArray[i], CoordArray[i+1]);
+                }
+
+                return extend;
+            }
+        }
+
+        private static double[] DoubleArrayFromCoordinateString(string coordinates)
+        {
+            var coordSplit = coordinates.Replace("[", "").Replace("]", "").Replace(" ", "").Split(',');
+
+            var coordArray = new double[coordSplit.Length];
+
+            for (int i = 0; i < coordSplit.Length; i++)
+            {
+                coordArray[i] = Double.Parse(coordSplit[i], CultureInfo.InvariantCulture);
+            }
+
+            return coordArray;
         }
     }
 }
